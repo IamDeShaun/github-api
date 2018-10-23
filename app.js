73f2e333c3d600062a1cb8b35ebee345641e7acf
DeShaun Jones <viceprezofnv@gmail.com>
2018-10-23 15:10:07 -0400

    App Update
    1) App.js - Create async call back to fetch data from Github api. Display data through the console log to test.
    2) Github.js - Deleted
    3) Updated inline css styles, deleted link to Github.js and Storage.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// Create Class and Constructor 
+class Github {
+
+  constructor() {
+    this.client_id = 'bc79a71d92029fa69223';
+    this.client_secret = '4e63f1f5495f78dbf39607c22fd1eb9b965866ff';
+  }
+
+  async getUser(){
+    const profileResponse = await fetch('https://api.github.com/users/iamdeshaun?client_id=${this.client_id}&client_secret=${this.client_secret}');
+
+    const profile = await profileResponse.json();
+
+    return {
+      profile
+    }
+  }
+}
+
+// Init Github
+const git = new Github;
+
+// Make http call
+git.getUser()
+.then(data => {
+  console.log(data);
+});
